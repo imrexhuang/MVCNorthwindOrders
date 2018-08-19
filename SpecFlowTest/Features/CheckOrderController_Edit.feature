d9c74c0f0830eba6c349ae60230cfa01ef0fa0ba
Rex <im@rex.tw>
2018-08-19 13:59:25 +0800

    TestCase use SpecFlow ref: https://dotblogs.com.tw/yc421206/2014/12/22/unit_test_specflow_hello_world

---
+++ SpecFlowTest/Features/CheckOrderController_Edit.feature
@@ -0,0 +1,9 @@
+﻿Feature: CheckOrderController_Edit
+	測試OrderController的Edit
+
+
+Scenario: Add two numbers
+	Given I have entered 50 into the calculator
+	And I have entered 70 into the calculator
+	When I press add
+	Then the result should be 120 on the screen
---
+++ SpecFlowTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpecFlowTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpecFlowTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("73abd35d-f83f-4d84-806d-685212670b0e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
